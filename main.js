42d2f186d8ca7f65c83f91031aa18c22e4e3ca22
katayama <tattu.0310@gmail.com>
2022-10-19 01:01:10 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+        items: [
+            { name: 'りんご', color: '赤色' },
+            { name: 'バナナ', color: '黄色' },
+            { name: 'すいか', color: '赤色' }
+        ]
+    }
+});

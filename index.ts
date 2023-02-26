8bffc83f7cae998964788f0fc3a510f117914c5f
katayama8000 <tattu.0310@gmail.com>
2023-02-26 16:48:17 +0900

    www

--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@ import { serve } from 'https://deno.land/std@0.89.0/http/server.ts';
 const s = serve({ port: 8000 });
 console.log('http://localhost:8000/');
 for await (const req of s) {
-  req.respond({ body: 'Hello World\n' });
+  req.respond({ body: 'Hello World!!' });
 }

7ccde77cf5f42afcd685cf5f0b4d6df8f0f7230c
katayama800 <tattu.0310@gmail.com>
2024-01-17 15:45:25 +0000

    add axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    http::StatusCode,
+    routing::{get, post},
+    Router,
+};
+
+use tokio;
+
+#[tokio::main]
+async fn main() -> Result<(), StatusCode> {
+    let app = Router::new().route("/", get(handler_get));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("Listening on: {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+    Ok(())
+}
+
+async fn handler_get() -> String {
+    println!("GET / html");
+    "Hello, World!".to_string()
 }

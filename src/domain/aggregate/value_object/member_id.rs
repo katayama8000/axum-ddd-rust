5850f1a4b5211bc60c5696e2737750bb9456258f
bouzuya <m@bouzuya.net>
2024-02-08 20:10:17 +0900

    Add impl From<MemberId> for usize

--- src/domain/aggregate/value_object/member_id.rs
+++ src/domain/aggregate/value_object/member_id.rs
@@ -21,3 +21,21 @@ impl fmt::Display for MemberId {
         write!(f, "{}", self.0)
     }
 }
+
+impl From<MemberId> for usize {
+    fn from(member_id: MemberId) -> Self {
+        member_id.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let member_id = MemberId::new(1);
+        assert_eq!(member_id.to_string(), "1");
+        assert_eq!(usize::from(member_id), 1);
+    }
+}

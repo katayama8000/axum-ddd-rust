61914daa5a435f6c1e10d61a9676d16be8908932
katayama800 <tattu.0310@gmail.com>
2024-02-11 04:00:45 +0000

    wip

--- src/usecase/fetch_circle.rs
+++ src/usecase/fetch_circle.rs
@@ -17,6 +17,13 @@ impl FetchCircleInput {
     }
 }
 
+#[derive(Debug, Deserialize)]
+pub struct FetchCircleOutput {
+    pub circle_id: usize,
+    pub circle_name: String,
+    pub owner_name: String,
+    // FIXME: add some fields
+}
 pub struct FetchCircleUsecase<T>
 where
     T: CircleRepositoryPort,
@@ -32,8 +39,18 @@ where
         FetchCircleUsecase { circle_repository }
     }
 
-    pub fn execute(&self, fetch_circle_input: FetchCircleInput) -> Result<Circle, Error> {
+    pub fn execute(
+        &self,
+        fetch_circle_input: FetchCircleInput,
+    ) -> Result<FetchCircleOutput, Error> {
         let circle_id = CircleId::new(fetch_circle_input.id);
-        self.circle_repository.find_circle_by_id(&circle_id)
+        self.circle_repository
+            .find_circle_by_id(&circle_id)
+            .map(|circle: Circle| FetchCircleOutput {
+                circle_id: usize::from(circle.id),
+                circle_name: circle.name,
+                owner_name: circle.owner.name,
+                // FIXME: add some fields
+            })
     }
 }

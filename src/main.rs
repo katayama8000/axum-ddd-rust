47f740a23b667e10ed7db7a0625218523feead64
katayama800 <tattu.0310@gmail.com>
2024-02-12 08:25:19 +0000

    minor fix

--- src/main.rs
+++ src/main.rs
@@ -159,7 +159,92 @@ mod tests {
         )?;
         assert_eq!(response_body, "Circle not found");
 
-        let response = app
+        // let response = app
+        //     .clone()
+        //     .oneshot(
+        //         axum::http::Request::builder()
+        //             .method("POST")
+        //             .uri("/circle")
+        //             .header(CONTENT_TYPE, "application/json")
+        //             .body(axum::body::Body::new(serde_json::to_string(
+        //                 &CreateCircleRequestBody {
+        //                     circle_name: "circle_name1".to_string(),
+        //                     capacity: 10,
+        //                     owner_name: "owner1".to_string(),
+        //                     owner_age: 21,
+        //                     owner_grade: 3,
+        //                     owner_major: "Music".to_string(),
+        //                 },
+        //             )?))?,
+        //     )
+        //     .await?;
+        // assert_eq!(response.status(), StatusCode::OK);
+
+        // let response_body = serde_json::from_slice::<CreateCircleResponseBody>(
+        //     &axum::body::to_bytes(response.into_body(), usize::MAX).await?,
+        // )?;
+
+        let response = build_circle(&app).await?;
+
+        let fetched_response = app
+            .oneshot(
+                axum::http::Request::builder()
+                    .method("GET")
+                    .uri(format!("/circle/{}", response.0))
+                    .body(axum::body::Body::empty())?,
+            )
+            .await?;
+        assert_eq!(fetched_response.status(), StatusCode::OK);
+        let fetched_response_body = String::from_utf8(
+            axum::body::to_bytes(fetched_response.into_body(), usize::MAX)
+                .await?
+                .to_vec(),
+        )?;
+        assert_eq!(
+            fetched_response_body,
+            format!(
+                "{{\"circle_id\":{},\"circle_name\":\"Music club\",\"capacity\":10,\"owner\":{{\"id\":{},\"name\":\"John Lennon\",\"age\":21,\"grade\":3,\"major\":\"Music\"}},\"members\":[]}}",
+                response.0, response.1
+            )
+        );
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_update_circle() -> anyhow::Result<()> {
+        let state = AppState {
+            circle_repository: CircleRepository::new(),
+        };
+        let app = router().with_state(state.clone());
+        let response = build_circle(&app).await?;
+        let update_response = app
+            .oneshot(
+                axum::http::Request::builder()
+                    .method("PUT")
+                    .uri(format!("/circle/{}", response.0))
+                    .header(CONTENT_TYPE, "application/json")
+                    .body(axum::body::Body::new(serde_json::to_string(
+                        &UpdateCircleRequestBody {
+                            circle_name: Some("Football club".to_string()),
+                            capacity: Some(20), // 例えば容量を20に更新
+                        },
+                    )?))?,
+            )
+            .await?;
+        assert_eq!(update_response.status(), StatusCode::OK);
+
+        // サークルが正しく更新されたことを確認します
+        let updated_circle = state
+            .circle_repository
+            .find_circle_by_id(&CircleId::new(response.0))?;
+        assert_eq!(updated_circle.name, "Football club");
+        assert_eq!(updated_circle.capacity, 20);
+
+        Ok(())
+    }
+
+    async fn build_circle(app: &Router) -> anyhow::Result<(usize, usize)> {
+        let create_response = app
             .clone()
             .oneshot(
                 axum::http::Request::builder()
@@ -168,9 +253,9 @@ mod tests {
                     .header(CONTENT_TYPE, "application/json")
                     .body(axum::body::Body::new(serde_json::to_string(
                         &CreateCircleRequestBody {
-                            circle_name: "circle_name1".to_string(),
+                            circle_name: "Music club".to_string(),
                             capacity: 10,
-                            owner_name: "owner1".to_string(),
+                            owner_name: "John Lennon".to_string(),
                             owner_age: 21,
                             owner_grade: 3,
                             owner_major: "Music".to_string(),
@@ -178,90 +263,14 @@ mod tests {
                     )?))?,
             )
             .await?;
-        assert_eq!(response.status(), StatusCode::OK);
-
-        let response_body = serde_json::from_slice::<CreateCircleResponseBody>(
-            &axum::body::to_bytes(response.into_body(), usize::MAX).await?,
+        assert_eq!(create_response.status(), StatusCode::OK);
+        let create_response_body = serde_json::from_slice::<CreateCircleResponseBody>(
+            &axum::body::to_bytes(create_response.into_body(), usize::MAX).await?,
         )?;
 
-        let response = app
-            .oneshot(
-                axum::http::Request::builder()
-                    .method("GET")
-                    .uri(format!("/circle/{}", response_body.circle_id))
-                    .body(axum::body::Body::empty())?,
-            )
-            .await?;
-        assert_eq!(response.status(), StatusCode::OK);
-        let fetched_response_body = String::from_utf8(
-            axum::body::to_bytes(response.into_body(), usize::MAX)
-                .await?
-                .to_vec(),
-        )?;
-        assert_eq!(
-            fetched_response_body,
-            format!(
-                "{{\"circle_id\":{},\"circle_name\":\"circle_name1\",\"capacity\":10,\"owner\":{{\"id\":{},\"name\":\"owner1\",\"age\":21,\"grade\":3,\"major\":\"Music\"}},\"members\":[]}}",
-                response_body.circle_id, response_body.owner_id
-            ) 
-        );
-        Ok(())
-    }
-
-    #[tokio::test]
-    async fn test_update_circle() -> anyhow::Result<()> {
-          let state = AppState {
-        circle_repository: CircleRepository::new(),
-    };
-    let app = router().with_state(state.clone());
-
-    let create_response = app.clone()
-        .oneshot(
-            axum::http::Request::builder()
-                .method("POST")
-                .uri("/circle")
-                .header(CONTENT_TYPE, "application/json")
-                .body(axum::body::Body::new(serde_json::to_string(
-                    &CreateCircleRequestBody {
-                        circle_name: "Music club".to_string(),
-                        capacity: 10,
-                        owner_name: "john Lennon".to_string(),
-                        owner_age: 21,
-                        owner_grade: 3,
-                        owner_major: "Music".to_string(),
-                    },
-                )?))?,
-        )
-        .await?;
-    assert_eq!(create_response.status(), StatusCode::OK);
-    let create_response_body = serde_json::from_slice::<CreateCircleResponseBody>(
-        &axum::body::to_bytes(create_response.into_body(), usize::MAX).await?,
-    )?;
-
-    let circle_id = create_response_body.circle_id;
-
-    let update_response = app
-        .oneshot(
-            axum::http::Request::builder()
-                .method("PUT")
-                .uri(format!("/circle/{}", circle_id))
-                .header(CONTENT_TYPE, "application/json")
-                .body(axum::body::Body::new(serde_json::to_string(
-                    &UpdateCircleRequestBody {
-                        circle_name:Some( "Football club".to_string()),
-                        capacity: Some(20), // 例えば容量を20に更新
-                    },
-                )?))?,
-        )
-        .await?;
-    assert_eq!(update_response.status(), StatusCode::OK);
-
-    let updated_circle = state
-        .circle_repository
-        .find_circle_by_id(&CircleId::new(circle_id))?;
-    assert_eq!(updated_circle.name, "Football club");
-    assert_eq!(updated_circle.capacity, 20);
-
-    Ok(())
+        Ok((
+            create_response_body.circle_id,
+            create_response_body.owner_id,
+        ))
     }
 }

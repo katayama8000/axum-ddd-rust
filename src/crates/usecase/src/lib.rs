59b08fb67ef522e742888bd07f51cfdaa3acd1ff
katayama8000 <63860688+katayama8000@users.noreply.github.com>
2024-06-15 08:29:19 +0900

    wip (#37)

--- src/crates/usecase/src/lib.rs
+++ src/crates/usecase/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod create_circle;
+pub mod fetch_all_circle;
 pub mod fetch_circle;
 pub mod update_circle;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use crate::{
     config::connect::connect,
-    handler::{handle_create_circle, handle_fetch_circle, handle_update_circle},
+    handler::{handle_create_circle, handle_fetch_all, handle_fetch_circle, handle_update_circle},
 };
 
 use axum::{
@@ -23,6 +23,7 @@ fn router() -> Router<AppState> {
     Router::new()
         .route("/", get(handle_get_version))
         .route("/circle/:id", get(handle_fetch_circle))
+        .route("/circle", get(handle_fetch_all))
         .route("/circle", post(handle_create_circle))
         .route("/circle/:id", put(handle_update_circle))
         .route("/test", get(handle_get_test))

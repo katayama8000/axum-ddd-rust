6196621a5b82e34e0ccb963bf7e783c38a4b9aeb
katayama800 <tattu.0310@gmail.com>
2024-10-05 09:09:33 +0000

    minor fix

--- src/crates/usecase/src/fetch_circle.rs
+++ src/crates/usecase/src/fetch_circle.rs
@@ -104,13 +104,13 @@ mod tests {
         let circle_id = CircleId::from_str("123")?;
         let owner = Member::new(
             "john".to_string(),
-            20,
+            21,
             Grade::try_from(3)?,
             Major::from("ComputerScience"),
         );
         let members = vec![Member::new(
             "mike".to_string(),
-            20,
+            19,
             Grade::try_from(1).unwrap(),
             Major::from("Economics"),
         )];
@@ -132,20 +132,20 @@ mod tests {
         let input = FetchCircleInput::new(circle_id.clone().into());
         let output = usecase.execute(input).await.unwrap();
 
-        // assert_eq!(output.circle_id, circle_id.to_string());
+        assert_eq!(output.circle_id, circle_id.to_string());
         assert_eq!(output.circle_name, "music");
         assert_eq!(output.capacity, 10);
         assert_eq!(output.owner.id, owner.id.to_string());
         assert_eq!(output.owner.name, "john");
-        assert_eq!(output.owner.age, 20);
+        assert_eq!(output.owner.age, 21);
         assert_eq!(output.owner.grade, 3);
         assert_eq!(output.owner.major, "ComputerScience");
         assert_eq!(output.members.len(), 1);
         assert_eq!(output.members[0].id, members[0].id.clone().to_string());
         assert_eq!(output.members[0].name, "mike");
-        assert_eq!(output.members[0].age, 20);
+        assert_eq!(output.members[0].age, 19);
         assert_eq!(output.members[0].grade, 1);
         assert_eq!(output.members[0].major, "Economics");
-        anyhow::Result::Ok(())
+        Ok(())
     }
 }

9806e9ab4cb768ff033527651e8d38be895b1675
katayama800 <tattu.0310@gmail.com>
2024-10-29 15:03:47 +0000

    Remove

--- src/crates/domain/src/aggregate/member.rs
+++ src/crates/domain/src/aggregate/member.rs
@@ -2,7 +2,7 @@ use super::value_object::{grade::Grade, major::Major, member_id::MemberId};
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Member {
-    pub id: MemberId, // メンバーのID (Value Object)
+    pub id: MemberId,
     pub name: String,
     pub age: i16,
     pub grade: Grade,
@@ -10,7 +10,6 @@ pub struct Member {
 }
 
 impl Member {
-    // メンバーの新規作成メソッド
     pub fn new(name: String, age: i16, grade: Grade, major: Major) -> Self {
         Member {
             id: MemberId::gen(),
@@ -21,7 +20,6 @@ impl Member {
         }
     }
 
-    // メンバーの再構成メソッド
     pub fn reconstruct(id: MemberId, name: String, age: i16, grade: Grade, major: Major) -> Self {
         Member {
             id,
@@ -32,7 +30,6 @@ impl Member {
         }
     }
 
-    // 20歳以上かどうかを判定するメソッド
     pub fn is_adult(&self) -> bool {
         self.age >= 20
     }

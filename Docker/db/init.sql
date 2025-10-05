5098eddf52ebbb6f4c130dbdb30c08bfc5290e83
katayama8000 <tattu.0310@gmail.com>
2025-10-05 11:33:31 +0900

    Refactor init.sql to remove Japanese comments for consistency

--- Docker/db/init.sql
+++ Docker/db/init.sql
@@ -1,13 +1,9 @@
--- データベースの作成
 CREATE DATABASE IF NOT EXISTS mydatabase;
 
--- myuserに全ての権限を付与
 GRANT ALL PRIVILEGES ON mydatabase.* TO 'myuser' @'%' IDENTIFIED BY 'mypassword';
 
--- mydatabaseを使用する
 USE mydatabase;
 
--- Circlesテーブルの作成
 CREATE TABLE IF NOT EXISTS circles (
     id CHAR(36) NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -15,7 +11,6 @@ CREATE TABLE IF NOT EXISTS circles (
     owner_id CHAR(36) NOT NULL
 );
 
--- Membersテーブルの作成
 CREATE TABLE IF NOT EXISTS members (
     id CHAR(36) NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -26,7 +21,6 @@ CREATE TABLE IF NOT EXISTS members (
     FOREIGN KEY (circle_id) REFERENCES circles(id) ON DELETE CASCADE
 );
 
--- Circlesテーブルへの初期データ挿入
 INSERT INTO
     circles (id, name, capacity, owner_id)
 VALUES
@@ -34,7 +28,6 @@ VALUES
     (UUID(), 'Circle B', 8, UUID()),
     (UUID(), 'Circle C', 10, UUID());
 
--- Membersテーブルへの初期データ挿入
 INSERT INTO
     members (id, name, grade, circle_id, age, major)
 VALUES
